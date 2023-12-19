cc89a981e4bdc00d791e87f0ea05e1f3b70a8439
caelansar <819711623@qq.com>
2023-12-19 22:21:39 +0800

    style: polish

--- src/dns.rs
+++ src/dns.rs
@@ -25,7 +25,7 @@ impl ResultCode {
             3 => ResultCode::NXDOMAIN,
             4 => ResultCode::NOTIMP,
             5 => ResultCode::REFUSED,
-            0 | _ => ResultCode::NOERROR,
+            _ => ResultCode::NOERROR,
         }
     }
 }
